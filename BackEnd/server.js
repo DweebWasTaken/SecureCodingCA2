724c2d9f97020ce6788d68fd8886ecb6a9951a4f
HobGoblin94 <amin.al.adeeb@gmail.com>
2023-01-30 12:20:44 +0800

    hi

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,16 +1,16 @@
+
+
 var express = require('express');
 var serveStatic = require('serve-static');
 var app = require('./controller/app.js');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
 
 var port = 8081;
 
-app.use(serveStatic(__dirname + '/public'));
+app.use(serveStatic(__dirname + '/public')); 
 
-var server = app.listen(port, function() {
+var server = app.listen(port, function(){
     console.log('Web App Hosted at http://localhost:%s', port);
 });
 
 var app = require('./controller/app.js');
+
--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -1,50 +1,31 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
+const express=require('express');
+const serveStatic=require('serve-static');
 
-var hostname = "localhost";
-var port = 3001;
+var hostname="localhost";
+var port=3001;
 
-var app = express();
+var app=express();
 
-app.use(function(req, res, next) {
+app.use(function(req,res,next){
     console.log(req.url);
     console.log(req.method);
     console.log(req.path);
     console.log(req.query.id);
 
-    if (req.method != "GET") {
+    if(req.method!="GET"){
         res.type('.html');
-        var msg = "<html><body>This server only serves web pages with GET!</body></html>";
+        var msg="<html><body>This server only serves web pages with GET!</body></html>";
         res.end(msg);
-    } else {
+    }else{
         next();
     }
 });
 
-/////////
-const sslServer = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-}, app)
+
+app.use(serveStatic(__dirname+"/public"));
 
 
-
-
-
-//var server = sslServer.listen(port, () => {console.log('HTTPS Server running on ', port);});
-
-
-
-
-///////////
-
-app.use(serveStatic(__dirname + "/public"));
-
-
-sslServer.listen(port, hostname, function() {
+app.listen(port,hostname,function(){
 
     console.log(`Server hosted at http://${hostname}:${port}`);
 });
