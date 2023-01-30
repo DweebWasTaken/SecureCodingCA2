66a82f5bb9604b71e8722410074b1ac5023cd103
HobGoblin94 <amin.al.adeeb@gmail.com>
2023-01-30 19:49:05 +0800

    hashed problem

--- BackEnd/model/user.js
+++ BackEnd/model/user.js
@@ -1,107 +1,130 @@
 var db = require('./databaseConfig.js');
 var config = require('../config.js');
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
 var userDB = {
 
-	loginUser: function (email, password, callback) {
+    loginUser: function(email, password, callback) {
 
-		var conn = db.getConnection();
+        var conn = db.getConnection();
 
-		conn.connect(function (err) {
-			if (err) {
-				console.log(err);
-				return callback(err, null);
-			}
-			else {
-				console.log("Connected!");
+        conn.connect(function(err) {
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                console.log("Connected!");
 
-				var sql = 'select * from users where email = ? and password=?';
-				conn.query(sql, [email, password], function (err, result) {
-					conn.end();
+                var sql = "SELECT * FROM user WHERE email = ?";
 
-					if (err) {
-						console.log("Err: " + err);
-						return callback(err, null, null);
+                conn.query(sql, [email], function(err, result) {
+                    if (err) {
 
-					} else {
-						var token = "";
+                        console.log(err);
+                        return callback(err, null);
+                    } else {
 
-						if (result.length == 1) {
-							token = jwt.sign({ id: result[0].id }, config.key, {
-								expiresIn: 86400 //expires in 24 hrs
-							});
-							console.log("@@token " + token);
-							return callback(null, token, result);
-						} //if(res)
-						else {
-							console.log("email/password does not match");
-							var err2 = new Error("Email/Password does not match.");
-							err2.statusCode = 404;
-							console.log(err2);
-							return callback(err2, null, null);
-						}
-					}  //else
-				});
-			}
-		});
-	},
+                        if (result.length == 1) { //1 rec retrieved since email is unique
 
-	updateUser: function (username, firstname, lastname, id, callback) {
+                            const hashedPassword = result[0].password;
+                            // Compare the provided password with the hashed password using bcrypt
+                            bcrypt.compare(password, hashedPassword, function(err, match) {
+                                if (err) {
+                                    console.log(err);
+                                    return callback(err, null);
+                                } else if (match) {
+                                    // passwords match
+                                    var role = result[0].role;
+                                    var username = result[0].username;
+                                    var token = jwt.sign({ "role": role, "username": username }, config.JWTKey, { expiresIn: 86400 });
 
-		var conn = db.getConnection();
-		conn.connect(function (err) {
-			if (err) {
-				console.log(err);
-				return callback(err, null);
-			} else {
-				console.log("Connected!");
+                                    console.log("It works | Password: " + password + " hashedass: " + hashedPassword);
+                                    return callback(null, token);
+                                } else {
+                                    // passwords do not match
+                                    return callback(null, null);
+                                }
+                            });
 
-				var sql = "update users set username = ?,firstname = ?,lastname = ? where id = ?;";
+                        } else {
 
-				conn.query(sql, [username, firstname, lastname, id], function (err, result) {
-					conn.end();
+                            //login wrong...
+                            return callback(null, null);
 
-					if (err) {
-						console.log(err);
-						return callback(err, null);
-					} else {
-						console.log("No. of records updated successfully: " + result.affectedRows);
-						return callback(null, result.affectedRows);
-					}
-				})
-			}
-		})
-	},
+                        }
 
-	addUser: function (username, email, password, profile_pic_url, role, callback) {
+                    }
 
-		var conn = db.getConnection();
+                });
 
-		conn.connect(function (err) {
-			if (err) {
-				console.log(err);
-				return callback(err, null);
-			} else {
+            }
+
+        });
+
+    },
 
 
-				console.log("Connected!");
-				var sql = "Insert into users(username,email,password,profile_pic_url,role) values(?,?,?,?,?)";
-				conn.query(sql, [username, email, password, profile_pic_url, role], function (err, result) {
-					conn.end();
 
-					if (err) {
-						console.log(err);
-						return callback(err, null);
-					} else {
-						return callback(null, result);
-					}
-				});
+    updateUser: function(username, firstname, lastname, id, callback) {
 
-			}
-		});
-	},
-};
+        var conn = db.getConnection();
+        conn.connect(function(err) {
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                console.log("Connected!");
+
+                var sql = "update users set username = ?,firstname = ?,lastname = ? where id = ?;";
+
+                conn.query(sql, [username, firstname, lastname, id], function(err, result) {
+                    conn.end();
+
+                    if (err) {
+                        console.log(err);
+                        return callback(err, null);
+                    } else {
+                        console.log("No. of records updated successfully: " + result.affectedRows);
+                        return callback(null, result.affectedRows);
+                    }
+                })
+            }
+        })
+    },
+
+    addUser: function(username, email, password, profile_pic_url, role, callback) {
+
+        var conn = db.getConnection();
+        bcrypt.hash(password, 10, function(err, hash) {
+            conn.connect(function(err) {
+                if (err) {
+                    console.log(err);
+                    return callback(err, null);
+                } else {
 
 
+                    console.log("Connected!");
+                    var sql = "Insert into users(username,email,password,profile_pic_url,role) values(?,?,?,?,?)";
+                    conn.query(sql, [username, email, hash, profile_pic_url, role], function(err, result) {
+                        conn.end();
+
+                        if (err) {
+                            console.log(err);
+                            return callback(err, null);
+                        } else {
+                            return callback(null, result);
+                        }
+
+                    })
+                }
+            })
+        })
+    }
+}
+
+
+
+
+module.exports = userDB;
 module.exports = userDB;
